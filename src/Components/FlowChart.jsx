bf22e4a6124904a1fea11dff547618bcc24db6e7
Eaarjun <91949706+Eaarjun@users.noreply.github.com>
2024-10-06 23:25:40 +0530

    Update FlowChart.jsx

--- src/Components/FlowChart.jsx
+++ src/Components/FlowChart.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import Flowchart from '../images/flowchart.png';
-
+import React from 'react'
+import Flowchart from '../images/flowchart.png'
 const FlowChart = () => {
+  const imgStyle = {
+    width: '100%', // Make image responsive
+    height: 'auto', // Maintain aspect ratio
+    maxWidth: '800px', // Limit max width for larger screens
+    margin: '0 auto', // Center image
+    display: 'block', // Center block element
+};
   return (
-    <>
-      <h2 style={{ marginTop: '3rem', textAlign: 'center', fontWeight: 500, fontSize: '3rem' }}>
-        Flowchart
-      </h2>
-      <div className='flowChartImage' style={{ display: 'flex', justifyContent: 'center' }}>
-        <img 
-          src={Flowchart} 
-          alt="Flowchart" 
-          style={{ width: '85%', height: 'auto', maxWidth: '600px' }} // Ensure it doesn't exceed a certain width
-        />
-      </div>
-    </>
-  );
+<>
+    <h2 style={{marginTop: '3rem',textAlign: 'center',fontWeight: 500,fontSize: '3rem'}}>Flowchart</h2> 
+    <div className='flowChartImage' >
+    <img src={Flowchart} alt="Flowchart" width='85%' height='auto' style={imgStyle} />
+    </div>
+
+</>
+  )
 }
 
-export default FlowChart;
+export default FlowChart
